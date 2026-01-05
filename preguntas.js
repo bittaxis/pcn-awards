21c5abf6a7e1be647e819ba4bca7250e98396f96
snunez <60078252+bittaxis@users.noreply.github.com>
2026-01-05 20:23:39 -0300

    pcn awards

---
+++ preguntas.js
@@ -0,0 +1,77 @@
+// preguntas.js
+
+export const SECCIONES = [
+    {
+        nombre: "Docentes y Ayudantes",
+        preguntas: [
+            { titulo: "La mejor guerrera versión docente" },
+            { titulo: "Peores PPTs de la historia" },
+            { titulo: "El Ayudante Legendario" },
+            { titulo: "Ayudante... ¿de verdad ayuda?" },
+            { titulo: "Frase iconica de un profe" },
+            { titulo: "El Reina del 'Visto, nunca responde los correos" },
+            { titulo: "Profe Motivador de la carrera:Que te hace querer ser mejor profesional" },
+            { titulo: "Profe flexibles:El milagro del semestre que entiende que somos humanos" },
+            { titulo: "Profe Zen:Nunca se altera con los atrasos" },
+            { titulo: "Profe más temido (pero queridx)" },
+        ]
+    },
+    {
+        nombre: "Vida Académica",
+        preguntas: [
+            { titulo: "Mejor Guerrero/a de la carrera" },
+            { titulo: "La Remontada Épica" },
+            { titulo: "Ramo Relleno" },
+            { titulo: "Ramo rompe-almas" },
+            { titulo: "Laboratorio del Terror" },
+            { titulo: "La Librería Ambulante, se sabe todas las referancias" },
+            { titulo: "Mejor salida pedagógica" },
+            { titulo: "Peor salida pedagógica" },
+        ]
+    },
+    {
+        nombre: "Personalidades del Grupo",
+        preguntas: [
+            { titulo: "Cerebro del grupo" },
+            { titulo: "Influencer del grupo" },
+            { titulo: "Organizadx del año: Tiene carpetas, subcarpetas y etiquetas de color" },
+            { titulo: "Drama King/Queen" },
+            { titulo: "Comediante Oficial" },
+            { titulo: "Estilo Impecable, viene a clases como si fuera pasarela" },
+            { titulo: "Modo exposición legendaria, presenta como si diera una TED talk" },
+            { titulo: "El o la más Borracho/cha" },
+            { titulo: "Archivista oficial, guarda todas las fotos y recuerdos" },
+            { titulo: "Reloj Mal Ajustado" },
+            { titulo: "Snack deales, siempre tiene algo para picar y/o compartir" },
+            { titulo: "Aprobó de Milagro" },
+            { titulo: "Riego economico, se gastó toda la juna del mes en la primera semana" },
+            { titulo: "Dios/a del PowerPoint" },
+            { titulo: "Entrega milagrosa todo a tiempo aunque empez+o anoche" },
+            { titulo: "Dúo o Trío Dinámico" },
+            { titulo: "La Mamá/Papá del Grupo, se preocupa que todos coman y entreguen" },
+            { titulo: "Premio 'Ya voy llegando'" },
+            { titulo: "Calculadora viviente, matemáticas y numeros nada se le escapa" },
+            { titulo: "La Risa del Año" },
+            { titulo: "Sobreviviente de los trabajos grupales, vivió para contarlo" },
+        ]
+    },
+    {
+        nombre: "Anécdotas y Memorias",
+        preguntas: [
+            { titulo: "Nicosas (Momentos Nico)" },
+            { titulo: "Peor actividad Explora" },
+            { titulo: "Salida memorable" },
+            { titulo: "La Disertación Eterna" },
+            { titulo: "La Crisis Colectiva" },
+            { titulo: "Mejor momento en clase" },
+            { titulo: "Mejor edit de la Emily" },
+            { titulo: "Momento iconico de la generación" }
+        ]
+    },
+    {
+        nombre: "Mencion honorifica",
+        preguntas: [
+            { titulo: "Por que unas votaciones no alcanzan para la inmensidad de historias que tienes para contar, ¿Por qué no haces una mencion honorifica? (Tambien puedes dejar un mensaje para compartir de tus sentimientos y/o emociones sobre estos 5 años) " },
+        ]
+    }
+];
